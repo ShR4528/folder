7fba5a10b88b4d04d8ccc47bc828409478985fe4
Shr4528 <Shr4528@gmail.com>
2023-01-26 18:02:43 +0200

    master2

---
+++ client/src/components/Woddy.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+function Woddy() {
+  return <div></div>
+}
+
+export default Woddy
---
+++ client/src/pages/House.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+function House() {
+  return <div></div>
+}
+
+export default House
